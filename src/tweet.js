20571d3e1c5f27e7d9163c5803df50cf2954201d
Arthur Sandro <arthurcs426@gmail.com>
2020-10-14 16:50:40 -0700

    created an object to hold our tweet information

--- src/tweet.js
+++ src/tweet.js
@@ -10,6 +10,18 @@ import {
   ShareButton,
 } from './tweetComponents';
 
+const tweetData = {
+  mesage: 'I like playing Apex Legends with the bois.',
+  gravatar: 'xyz',
+  author: {
+    handle: 'arthur14',
+    name: 'Arthur'
+  },
+  likes: 2,
+  retweets: 0,
+  timestamp: '2020-10-14 4:50:04',
+};
+
 const Tweet = () => {
   return (
     <div className="tweet">
