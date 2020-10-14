eef9bef562bbbe9ae3a0bf285f8be4d5cdff18c1
Arthur Sandro <arthurcs426@gmail.com>
2020-10-13 17:47:06 -0700

    added some components to create our static tweet along with styling

---
+++ src/tweet.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Avatar from './avatar';
+
+const Tweet = () => {
+  return (
+    <div className="tweet">
+      <Avatar />
+    </div>
+  );
+}
+
+export default Tweet;
