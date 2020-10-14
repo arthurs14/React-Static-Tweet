7d2710c8a4c3c4038d75a17abcb35e7479520ee3
Arthur Sandro <arthurcs426@gmail.com>
2020-10-14 16:58:47 -0700

    put tweet data into its own javascript file and added the data to main component

--- src/tweet.js
+++ src/tweet.js
@@ -10,19 +10,12 @@ import {
   ShareButton,
 } from './tweetComponents';
 
-const tweetData = {
-  mesage: 'I like playing Apex Legends with the bois.',
-  gravatar: 'xyz',
-  author: {
-    handle: 'arthur14',
-    name: 'Arthur'
-  },
-  likes: 2,
-  retweets: 0,
-  timestamp: '2020-10-14 4:50:04',
-};
+import tweetData from './tweetData';
 
 const Tweet = () => {
+  const { message, gravatar, author, likes,
+    retweets, timestamp } = tweetData;
+
   return (
     <div className="tweet">
       <Avatar />
---
+++ src/tweetData.js
@@ -0,0 +1,13 @@
+const tweetData = {
+  message: 'I like playing Apex Legends with the bois.',
+  gravatar: 'xyz',
+  author: {
+    handle: 'arthur14',
+    name: 'Arthur'
+  },
+  likes: 2,
+  retweets: 0,
+  timestamp: '2020-10-14 4:50:04',
+};
+
+export default tweetData;
