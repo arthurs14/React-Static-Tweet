3b2fbaff077dcd10c90f1c3d34401300257bed84
Arthur Sandro <arthurcs426@gmail.com>
2020-10-13 17:55:32 -0700

    added components for message and name handle along with styling for those components

--- src/tweet.js
+++ src/tweet.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Avatar from './avatar';
+import Message from './message';
+import NameWithHandle from './nameWithHandle';
 
 const Tweet = () => {
   return (
     <div className="tweet">
       <Avatar />
+      <div className="content">
+        <NameWithHandle />
+        <Message />
+      </div>
     </div>
   );
 }
